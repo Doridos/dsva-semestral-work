b9354fa3051aa22208226b49853756308c03cc30
Vladyslav Babyč <babycvla@fel.cvut.cz>
2023-12-26 22:22:44 +0100

    functional addition and disconnect of node

--- src/main/java/cz/cvut/fel/dsva/MessageSubscriber.java
+++ src/main/java/cz/cvut/fel/dsva/MessageSubscriber.java
@@ -12,6 +12,7 @@ public class MessageSubscriber {
 	private static HashMap<String, Boolean> Req = new HashMap<>();
 	private static String ID;
 	private static boolean rebuild = false;
+	private static Timer timer = null;
 
 
 	public static void main(String[] args) {
@@ -86,6 +87,13 @@ public class MessageSubscriber {
 							parts = messageText.split("\\|");
 							keyToWrite = parts[1];
 							valueToStore = parts[2];
+
+							if(timer != null){
+								timer.cancel();
+							}
+							timer = new Timer();
+							timer.schedule(new RebuildTopology(), 5000);
+
 						} else {
 							message = topicConsumer.receiveNoWait();
 							if (message != null) {
@@ -96,6 +104,7 @@ public class MessageSubscriber {
 							}
 						}
 					} else {
+
 						message = topicConsumer.receive();
 						TextMessage txtMsg = (TextMessage) message;
 						messageText = txtMsg.getText();
@@ -145,8 +154,6 @@ public class MessageSubscriber {
 					}
 
 					if (Objects.equals(RpCnt, Req.size() - 1) && keyToWrite != null && !rebuild) {
-
-
 						dataStore.put(keyToWrite, valueToStore);
 						System.out.println(MyRq + "Wrote to key number: " + keyToWrite + " value of:" + valueToStore);
 						numberOfOperations += 1;
@@ -179,6 +186,42 @@ public class MessageSubscriber {
 			}
 		}
 
+	}
+
+	static class RebuildTopology extends TimerTask {
+		@Override
+		public void run() {
+			System.out.println("REBUILDING");
+			// Clear the Req HashMap
+			Req.clear();
+			Req.put(String.valueOf(ID), false);
+			rebuild = true;
+			// Send a different message to the topic
+			try {
+				// #### administered object ####
+				// This statement can be eliminated if JNDI is used.
+				ConnectionFactory connectionFactory = new com.sun.messaging.ConnectionFactory();
+				((com.sun.messaging.ConnectionFactory) connectionFactory).setProperty(ConnectionConfiguration.imqAddressList, "mq://192.168.18.44:7676,mq://192.168.18.44:7677");
+				// Create a connection to the JMS
+				Connection myConn = connectionFactory.createConnection();
+				// Create a session within the connection.
+				Session topicSession = myConn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+				// Instantiate a JMS Queue Destination
+				// This statement can be eliminated if JNDI is used.
+				//Topic for communication
+				Destination topicRiAg = topicSession.createTopic("RiAgTopic");
+				// #### Client ####
+				// Create a message consumer.
+				MessageProducer topicProducer = topicSession.createProducer(topicRiAg);
+				TextMessage helloMessage = topicSession.createTextMessage();
+				helloMessage.setText("Start|" + ID);
+				topicProducer.send(helloMessage);
+				rebuild = false;
+
+			} catch (JMSException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
 
